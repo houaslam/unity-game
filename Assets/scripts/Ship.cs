d8b45ffecb1055884463654a394909f4c55145fc
tahaaitbaissi <tahaaitbaissi666@gmail.com>
2024-01-24 16:52:53 +0100

    Basic Ship Movement

---
+++ Assets/scripts/Ship.cs
@@ -0,0 +1,48 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float thrustSpeed = 1.0f;
+    public float turnSpeed = 0.5f;
+
+    private Rigidbody2D rb;
+    private bool thrusting;
+    private float turnDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))   
+        {
+            turnDirection = 1.0f;
+        } 
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnDirection = -1.0f;
+        }   
+        else
+        {
+            turnDirection = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrusting)
+        {
+            rb.AddForce(this.transform.up * this.thrustSpeed);
+        }
+
+        if (turnDirection != 0.0f)
+        {
+            rb.AddTorque(turnDirection * turnSpeed);
+        }
+    }
+}
